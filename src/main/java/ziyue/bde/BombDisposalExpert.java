b12cfe7193234e745bf09b79385ae51e9e2a14fe
子悦解说 <ziyuecommentary@gmail.com>
2025-05-05 18:38:22 +0800

    Flammable

--- src/main/java/ziyue/bde/BombDisposalExpert.java
+++ src/main/java/ziyue/bde/BombDisposalExpert.java
@@ -1,11 +1,15 @@
 package ziyue.bde;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -23,18 +27,34 @@ import org.apache.logging.log4j.Logger;
  */
 
 @Mod(BombDisposalExpert.MOD_ID)
-public class BombDisposalExpert {
+public class BombDisposalExpert
+{
     public static final String MOD_ID = "bde";
     public static final Logger LOGGER = LogManager.getLogger();
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final RegistryObject<Block> TNT_NO_GUNPOWDER = BLOCKS.register("tnt_no_gunpowder", () -> new Block(BlockBehaviour.Properties.copy(Blocks.TNT)));
+    public static final RegistryObject<Block> TNT_NO_GUNPOWDER = BLOCKS.register("tnt_no_gunpowder", () -> new Block(BlockBehaviour.Properties.copy(Blocks.TNT))
+    {
+        @Override
+        public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+            return true;
+        }
+
+        @Override
+        public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+            return 10;
+        }
+
+        @Override
+        public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+            return 5;
+        }
+    });
     public static final RegistryObject<Item> TNT_NO_GUNPOWDER_ITEM = ITEMS.register("tnt_no_gunpowder", () -> new BlockItem(TNT_NO_GUNPOWDER.get(), new Item.Properties()));
 
-    public BombDisposalExpert(FMLJavaModLoadingContext context)
-    {
+    public BombDisposalExpert(FMLJavaModLoadingContext context) {
         LOGGER.info("Bomb Disposal Expert! Made by ZiYueCommentary.");
 
         IEventBus modEventBus = context.getModEventBus();
@@ -47,8 +67,7 @@ public class BombDisposalExpert {
         modEventBus.addListener(this::addCreative);
     }
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS)
             event.accept(TNT_NO_GUNPOWDER_ITEM);
     }
