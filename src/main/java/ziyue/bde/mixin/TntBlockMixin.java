6c956771906273f6c722dc082520753f41771f7e
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-06-02 20:31:25 +0800

    Datadrive
    使可以拆弹的物品数据驱动

--- src/main/java/ziyue/bde/mixin/TntBlockMixin.java
+++ src/main/java/ziyue/bde/mixin/TntBlockMixin.java
@@ -36,7 +36,7 @@ public abstract class TntBlockMixin extends Block
 
     @Inject(at = @At("HEAD"), method = "useItemOn", cancellable = true)
     private void beforeOnUse(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult p_330020_, CallbackInfoReturnable<InteractionResult> cir) {
-        if (player.isHolding(Items.SHEARS)) {
+        if (player.getItemInHand(hand).is(BombDisposalExpert.DEFUSER)) {
             level.setBlock(pos, BombDisposalExpert.TNT_NO_GUNPOWDER.get().defaultBlockState(), 11);
             level.playSound(null, pos, SoundEvents.SHEEP_SHEAR, SoundSource.BLOCKS);
             level.addFreshEntity(new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, new ItemStack(Items.GUNPOWDER)));
