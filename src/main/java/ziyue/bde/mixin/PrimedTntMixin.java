c95f34d4e6d54624d574cbc60d901da86d5b42d3
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-06-02 20:31:25 +0800

    Datadrive
    使可以拆弹的物品数据驱动

--- src/main/java/ziyue/bde/mixin/PrimedTntMixin.java
+++ src/main/java/ziyue/bde/mixin/PrimedTntMixin.java
@@ -30,7 +30,7 @@ public abstract class PrimedTntMixin extends Entity implements TraceableEntity
     // No @Inject required.
     @Override
     public InteractionResult interact(Player player, InteractionHand hand) {
-        if (player.isHolding(Items.SHEARS)) {
+        if (player.getItemInHand(hand).is(BombDisposalExpert.DEFUSER)) {
             player.getItemInHand(hand).hurtAndBreak(1, player, getSlotForHand(hand));
             this.spawnAtLocation(new ItemStack(BombDisposalExpert.TNT_NO_GUNPOWDER.get()));
             this.kill();
