47dc77a3fe9b4e465f002b2762798d3220804683
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-06-02 20:36:56 +0800

    Import

--- src/main/java/ziyue/bde/mixin/PrimedTntMixin.java
+++ src/main/java/ziyue/bde/mixin/PrimedTntMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.world.entity.TraceableEntity;
 import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import ziyue.bde.BombDisposalExpert;
