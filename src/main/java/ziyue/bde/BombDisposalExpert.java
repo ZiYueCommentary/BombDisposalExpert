47c7ec74879d7f40ef13b0ede7d8b30a08cf3c8d
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-06-04 16:21:18 +0800

    fix

--- src/main/java/ziyue/bde/BombDisposalExpert.java
+++ src/main/java/ziyue/bde/BombDisposalExpert.java
@@ -5,6 +5,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
