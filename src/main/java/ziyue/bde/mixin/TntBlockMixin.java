61219a21ac7f848f47ff31e544a660ed99e08376
低耐久铁砧 <67966070+Ironnoob73@users.noreply.github.com>
2025-06-06 22:39:55 +0800

    Datadrive for fabric 1.21.1 (#9)
    * Datadrive
    
    * Update gradle.properties
    
    ---------
    
    Co-authored-by: ZiYueCommentary <ziyuecommentary@gmail.com>

--- src/main/java/ziyue/bde/mixin/TntBlockMixin.java
+++ src/main/java/ziyue/bde/mixin/TntBlockMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
@@ -36,7 +35,7 @@ public abstract class TntBlockMixin extends Block
 
 	@Inject(at = @At("HEAD"), method = "onUseWithItem", cancellable = true)
 	private void beforeOnUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ItemActionResult> cir) {
-		if (player.isHolding(Items.SHEARS)) {
+		if (player.getStackInHand(hand).isIn(BombDisposalExpert.DEFUSER)) {
 			world.setBlockState(pos, BombDisposalExpert.TNT_NO_GUNPOWDER.getDefaultState());
 			world.playSound(null, pos, SoundEvents.ENTITY_SHEEP_SHEAR, SoundCategory.BLOCKS);
 			world.spawnEntity(new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, new ItemStack(Items.GUNPOWDER)));
